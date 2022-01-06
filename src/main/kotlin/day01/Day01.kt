7648917c6c1978604f18ec07d15e1759feab5117
Eric Tobin <eatobin@gmail.com>
2022-01-06 14:31:46 -0600

    Day01Test.kt tests

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -8,21 +8,21 @@ val inputString: String = bufferedReader.use { it.readText() }
 val gasList: List<Int> = inputString.trim().lines().map { it.toInt() }
 
 // part a
-fun gas(m: Int): Int = (m / 3) - 2
+fun fuel(m: Int): Int = (m / 3) - 2
 
-val answer: Int = gasList.sumOf { gas(it) }
+val answer: Int = gasList.sumOf { fuel(it) }
 
 // part b
-tailrec fun gasPlus(m: Int, accum: Int = 0): Int {
-    val newGas: Int = gas(m)
+tailrec fun fuelPlus(m: Int, accum: Int = 0): Int {
+    val newGas: Int = fuel(m)
     return if (newGas <= 0) {
         accum
     } else {
-        gasPlus(newGas, accum + newGas)
+        fuelPlus(newGas, accum + newGas)
     }
 }
 
-val answer2: Int = gasList.sumOf { gasPlus(it) }
+val answer2: Int = gasList.sumOf { fuelPlus(it) }
 
 fun main() {
     println("Answer Part A: $answer") // 3337766
---
+++ src/test/kotlin/Day01Test.kt
@@ -0,0 +1,21 @@
+import day01.fuel
+import day01.fuelPlus
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day01Test {
+    @Test
+    fun testFuel() {
+        assertEquals(2, fuel(12))
+        assertEquals(2, fuel(14))
+        assertEquals(654, fuel(1969))
+        assertEquals(33583, fuel(100756))
+    }
+
+    @Test
+    fun testFuelPlus() {
+        assertEquals(2, fuelPlus(14))
+        assertEquals(966, fuelPlus(1969))
+        assertEquals(50346, fuelPlus(100756))
+    }
+}
