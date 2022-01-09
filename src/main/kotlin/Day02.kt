8a99533fb58b04f4a65e064663235f44f9e522ad
Eric Tobin <eatobin@gmail.com>
2022-01-09 17:49:48 -0600

    2 progress

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,3 +1,6 @@
+import java.io.BufferedReader
+import java.io.File
+
 typealias Memory = Map<Int, Int>
 typealias Instruction = Map<Char, Int>
 
@@ -7,3 +10,13 @@ const val offsetB = 2
 const val offsetA = 3
 
 data class IntCode(val pointer: Int, val memory: Memory)
+
+fun makeMemory(file: String): Memory {
+    val bufferedReader: BufferedReader = File(fp).bufferedReader()
+    val intList: List<Int> =
+        bufferedReader
+            .use { it.readText() }
+            .trim().split(",")
+            .map { it.toInt() }
+    return intList.zip(0..intList.size).toMap()
+}
