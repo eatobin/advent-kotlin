5b2479dcfb0b580e5b921ea48b045df45269fdee
Eric Tobin <eatobin@gmail.com>
2022-01-10 15:49:05 -0600

    pad5

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -8,8 +8,6 @@ const val offsetC = 1
 const val offsetB = 2
 const val offsetA = 3
 
-data class IntCode(val pointer: Int, val memory: Memory)
-
 fun makeMemory(file: String): Memory {
     val bufferedReader = File(file).bufferedReader()
     val intList =
@@ -32,6 +30,29 @@ fun charToInt(aChar: Byte): Int {
 fun pad5(op: Int): Instruction {
     val inInts = "%05d".format(op).toByteArray().map { charToInt(it) }
     return listOf('a', 'b', 'c', 'd', 'e').zip(inInts).toMap()
+}
+
+data class IntCode(val pointer: Int, val memory: Memory)
+
+fun aParam(instruction: Instruction, ic: IntCode): Int {
+    return when (instruction['a']) {
+        0 -> ic.memory[ic.pointer + offsetA]!! // a-p-w
+        else -> 99
+    }
+}
+
+fun bParam(instruction: Instruction, ic: IntCode): Int {
+    return when (instruction['b']) {
+        0 -> ic.memory[ic.memory[ic.pointer + offsetB]]!! // b-p-r
+        else -> 99
+    }
+}
+
+fun cParam(instruction: Instruction, ic: IntCode): Int {
+    return when (instruction['c']) {
+        0 -> ic.memory[ic.memory[ic.pointer + offsetC]]!! // c-p-r
+        else -> 99
+    }
 }
 
 fun main() {
