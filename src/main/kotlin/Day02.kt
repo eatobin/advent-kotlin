452e64c3fffbe2597a058dac534eae83e779bf00
Eric Tobin <eatobin@gmail.com>
2022-01-14 17:17:22 -0600

    in process 02

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-typealias Memory = Map<Int, Int>
+typealias Memory = MutableMap<Int, Int>
 typealias Instruction = Map<Char, Int>
 
 const val fp = "src/main/resources/day02.csv"
@@ -11,7 +11,7 @@ const val offsetA = 3
 fun makeMemory(file: String): Memory {
     val bufferedReader = File(file).bufferedReader()
     val intList = bufferedReader.use { it.readText() }.trim().split(",").map { it.toInt() }
-    return (intList.indices).zip(intList).toMap()
+    return (intList.indices).zip(intList).toMap().toMutableMap()
 }
 
 fun charToInt(aChar: Byte): Int {
@@ -27,7 +27,7 @@ fun pad5(op: Int): Instruction {
     return listOf('a', 'b', 'c', 'd', 'e').zip(inInts).toMap()
 }
 
-data class IntCode(val pointer: Int, val memory: Memory)
+data class IntCode(var pointer: Int, var memory: Memory)
 
 fun aParam(instruction: Instruction, ic: IntCode): Int {
     var param = 0
@@ -53,72 +53,68 @@ fun cParam(instruction: Instruction, ic: IntCode): Int {
     return param
 }
 
-fun updateMemory(memory: Memory, index: Int, value: Int): Memory {
-    val mMemory = memory.toMutableMap()
-    mMemory[index] = value
-    return mMemory.toMap()
-}
+//fun updateMemory(memory: Memory, index: Int, value: Int): Memory {
+//    val mMemory = memory.toMutableMap()
+//    mMemory[index] = value
+//    return mMemory.toMap()
+//}
 
-tailrec fun opCode(ic: IntCode): IntCode {
+fun opCode(ic: IntCode): Int {
     val instruction: Instruction = pad5(ic.memory[ic.pointer]!!)
     if (instruction['d'] == 9) {
-        return ic
+        return 0
     } else {
-        when (instruction['e']) {
-            1 -> return opCode(
-                IntCode(
-                    pointer = ic.pointer + 4, memory = updateMemory(
-                        ic.memory, aParam(instruction, ic), bParam(instruction, ic) + cParam(instruction, ic)
-                    )
-                )
-            )
-            2 -> return opCode(
-                IntCode(
-                    pointer = ic.pointer + 4, memory = updateMemory(
-                        ic.memory, aParam(instruction, ic), bParam(instruction, ic) * cParam(instruction, ic)
-                    )
-                )
-            )
+        return when (instruction['e']) {
+            1 -> {
+                ic.memory[aParam(instruction, ic)] = bParam(instruction, ic) + cParam(instruction, ic)
+                ic.pointer += 4
+                1
+            }
+            2 -> {
+                ic.memory[aParam(instruction, ic)] = bParam(instruction, ic) * cParam(instruction, ic)
+                ic.pointer += 4
+                1
+            }
             else -> throw Exception("opcode is not valid")
         }
     }
 }
 
-fun updatedMemory(memory: Memory, noun: Int, verb: Int): Memory {
-    val mMemory = memory.toMutableMap()
-    mMemory[1] = noun
-    mMemory[2] = verb
-    return mMemory.toMap()
-}
-
-fun nounVerb(): Int {
-    var noun = 0
-    var verb = 0
-
-    out@ while (noun < 101) {
-        while (verb < 101) {
-            val tv = makeMemory(fp)
-            val candidate =
-                opCode(IntCode(pointer = 0, memory = updatedMemory(memory = tv, noun = noun, verb = verb))).memory[0]!!
-            if (candidate == 19690720) {
-                break@out
-            }
-            verb++
-        }
-        noun++
-        verb = 0
-    }
-    return (100 * noun) + verb
-}
-
-fun main() {
-    // part A
-    val memory = makeMemory(fp)
-    val ic: IntCode = opCode(IntCode(pointer = 0, memory = updatedMemory(memory = memory, noun = 12, verb = 2)))
-    val answer: Int = ic.memory[0]!!
-    println("Answer Part A: $answer") // 2890696
-
-    // part B
-    val answer2: Int = nounVerb()
-    println("Answer Part B: $answer2") // 8226
-}
+//fun updatedMemory(memory: Memory, noun: Int, verb: Int): Memory {
+//    val mMemory = memory.toMutableMap()
+//    mMemory[1] = noun
+//    mMemory[2] = verb
+//    return mMemory.toMap()
+//}
+//
+//fun nounVerb(): Int {
+//    var noun = 0
+//    var verb = 0
+//
+//    out@ while (noun < 101) {
+//        while (verb < 101) {
+//            val tv = makeMemory(fp)
+//            val candidate =
+//                opCode(IntCode(pointer = 0, memory = updatedMemory(memory = tv, noun = noun, verb = verb))).memory[0]!!
+//            if (candidate == 19690720) {
+//                break@out
+//            }
+//            verb++
+//        }
+//        noun++
+//        verb = 0
+//    }
+//    return (100 * noun) + verb
+//}
+//
+//fun main() {
+//    // part A
+//    val memory = makeMemory(fp)
+//    val ic: IntCode = opCode(IntCode(pointer = 0, memory = updatedMemory(memory = memory, noun = 12, verb = 2)))
+//    val answer: Int = ic.memory[0]!!
+//    println("Answer Part A: $answer") // 2890696
+//
+//    // part B
+//    val answer2: Int = nounVerb()
+//    println("Answer Part B: $answer2") // 8226
+//}
